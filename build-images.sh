b8b63085500d46b8dfa50b97f4791a99c8ea7a26
Kemboi Elvis <kemboielvis22@gmail.com>
2025-11-13 03:30:57 +0300

    moved to postgres 17

--- build-images.sh
+++ build-images.sh
@@ -46,7 +46,7 @@ buildah config --entrypoint=/ \
 	--label="org.nethserver.authorizations=traefik@node:routeadm" \
 	--label="org.nethserver.tcp-ports-demand=1" \
 	--label="org.nethserver.rootfull=0" \
-	--label="org.nethserver.images=docker.io/postgres:17-alpine ghcr.io/zitadel/zitadel:${APP_VERSION}" \
+	--label="org.nethserver.images=docker.io/postgres:17 ghcr.io/zitadel/zitadel:${APP_VERSION}" \
 	"${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
